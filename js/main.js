46dfa9d51baac5be5f97652765f5a0840a5d672f
RRCN08 <43510157+RRCN08@users.noreply.github.com>
2020-07-26 00:24:44 +0530

    v0.7

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
-            var sideslider = $('[data-toggle=collapse-side]');
-            var sel = sideslider.attr('data-target');
-            var sel2 = sideslider.attr('data-target-2');
-            sideslider.click(function(event){
-                $(sel).toggleClass('in');
-                $(sel2).toggleClass('out');
+
+            $("#footer").load("footer.html");
+
+            //ScrollOut
+            ScrollOut();
+
+            //Rellax Parallax Effect
+            var rellax = new Rellax('.rellax');
+
+            //Hamburger Menu
+            $('.third-button').on('click', function () {
+
+              $('.animated-icon').toggleClass('open');
             });
 
             // Add smooth scrolling to all links
             $("a").on('click', function(event) {
 
               // Make sure this.hash has a value before overriding default behavior
-              if (this.hash !== "" && this.hash !== "#projects-carousel") {
+              if (this.hash !== "" && this.hash !== ".carousel") {
                 console.log(this.hash);
                 // Prevent default anchor click behavior
                 event.preventDefault();
@@ -30,4 +37,42 @@ $(document).ready(function() {
                 });
               } // End if
             });
-        });
+
+            $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+
+            // detect scroll top or down
+            if ($('.smart-scroll').length > 0) { // check if element exists
+                var last_scroll_top = 0;
+                $(window).on('scroll', function() {
+                    scroll_top = $(this).scrollTop();
+                    if(scroll_top < last_scroll_top) {
+                        $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+                    }
+                    else {
+                        $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+                    }
+                    last_scroll_top = scroll_top;
+                });
+            }
+
+
+            //lax.js
+            window.onload = function() {
+            	lax.setup() // init
+
+            	const updateLax = () => {
+            		lax.update(window.scrollY)
+            		window.requestAnimationFrame(updateLax)
+            	}
+
+            	window.requestAnimationFrame(updateLax)
+            }
+
+            //Modal
+            $(document).ready(function () {
+                    $('#myModal').on('show.bs.modal', function (e) {
+                        var image = $(e.relatedTarget).attr('src');
+                        $(".img-responsive").attr("src", image);
+                    });
+            });
+          });
