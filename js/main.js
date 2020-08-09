a39c12dca7c85380d5e58fd6c4aa4b68c7768c82
RRCN08 <43510157+RRCN08@users.noreply.github.com>
2020-08-09 14:09:16 +0530

    parallax fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-            //$("#footer").load("footer.html");
+            $("#footer").load("footer.html");
 
             //Venobox
             $('.venobox').venobox();
@@ -20,28 +20,12 @@ $(document).ready(function() {
             });
 
             // Add smooth scrolling to all links
-            $("a").on('click', function(event) {
-
-              // Make sure this.hash has a value before overriding default behavior
-              if (this.hash !== "" && this.hash == ".carousel") {
-                console.log(this.hash);
-                // Prevent default anchor click behavior
-                event.preventDefault();
-
-                // Store hash
-                var hash = this.hash;
-
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                  scrollTop: $(hash).offset().top
-                }, 800, function(){
-
-                  // Add hash (#) to URL when done scrolling (default click behavior)
-                  window.location.hash = hash;
-                });
-              } // End if
+            $("#projects").click(function() {
+                $('html,body').animate({
+                    scrollTop: $("#Projects-Carousel").offset().top},
+                    'slow');
             });
+
 
             $('body').css('padding-top', $('.navbar').outerHeight() + 'px')
 
