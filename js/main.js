2758d656df72804462baf25ab6447f94cc4030a1
RRCN08 <43510157+RRCN08@users.noreply.github.com>
2021-10-24 19:06:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-$(document).ready(function($) {
-    var Body = $('body');
-    Body.addClass('preloader-site');
-});
-
-$(window).on('load', function() {
-    $('.preloader-wrapper').fadeOut();
-    $('body').removeClass('preloader-site');
-});
-
 $(document).ready(function() {
+
+            var Body = $('body');
+            Body.addClass('preloader-site');
+
+            $(window).on('load', function() {
+              $('.preloader-wrapper').fadeOut();
+              $('body').removeClass('preloader-site');
+            });
 
             $(function () {
             $(document).scroll(function () {
