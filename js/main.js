751f091889bcd3e482c8bc308449cc37577a54c3
RRCN08 <43510157+RRCN08@users.noreply.github.com>
2021-10-24 19:14:16 +0530

    preloader

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 $(document).ready(function() {
 
-            var Body = $('body');
-            Body.addClass('preloader-site');
-
-            $(window).on('load', function() {
-              $('.preloader-wrapper').fadeOut();
-              $('body').removeClass('preloader-site');
-            });
-
             $(function () {
             $(document).scroll(function () {
               var $nav = $(".navbar-fixed-top");
